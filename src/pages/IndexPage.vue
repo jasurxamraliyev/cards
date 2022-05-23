771c5b0c6351c026d549dd7a22fe7bcbaee17b89
jasurxamraliyev <jasjasurbek4@gmail.com>
2022-05-23 16:47:10 +0300

    card4

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,7 +5,7 @@
     <EssentialLink/><br>
     <Card2/><br>
     <Card3/>
-
+    <Card4/>
 
     <div class="q-mt-xl"></div>
   </div>
@@ -17,8 +17,9 @@ import Card1 from '../components/card1.vue';
 import EssentialLink from 'src/components/EssentialLink.vue';
 import Card2 from '../components/card2.vue';
 import Card3 from '../components/card3.vue';
+import Card4 from 'src/components/card4.vue';
 export default {
-  components: { card, Card1, EssentialLink, Card2, Card3 },
+  components: { card, Card1, EssentialLink, Card2, Card3, Card4 },
   component:{card}
 }
 </script>
